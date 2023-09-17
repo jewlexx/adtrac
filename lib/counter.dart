b4c5aac6b464793a6fba9c43d287105672201b82
Juliette Cordor <juliette@jewelexx.com>
2023-09-17 19:25:21 +1000

    fixes

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
 import 'package:flutter/material.dart';
 
 import 'common.dart';
@@ -11,13 +12,20 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<CounterPage> {
-  final Future<Counter> _vapeCount = Counter.init();
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _vapeCount,
-        builder: (BuildContext ctx, AsyncSnapshot<Counter> snapshot) {
+    var uid = FirebaseAuth.instance.currentUser?.uid;
+
+    if (!uid) {
+      // TODO: Login page;
+      return null;
+    }
+
+    var counter = Counter(uid);
+
+    return FutureBuilder<int>(
+        future: counter.count,
+        builder: (ctx, snapshot) {
           if (snapshot.data == null) {
             return Scaffold(
               appBar: AppBar(
@@ -27,7 +35,7 @@ class _CounterPageState extends State<CounterPage> {
             );
           }
 
-          var counter = snapshot.data!;
+          var count = snapshot.data!;
 
           return Scaffold(
             body: Center(
@@ -38,11 +46,13 @@ class _CounterPageState extends State<CounterPage> {
                     'Hits Today:',
                   ),
                   Text(
-                    "${counter.count}",
+                    "$count",
                     style: Theme.of(context).textTheme.headlineMedium,
                   ),
                   ElevatedButton.icon(
-                    onPressed: () => setState(() => counter.increment()),
+                    onPressed: () => setState(() async {
+                      await counter.increment();
+                    }),
                     icon: const Icon(Icons.add),
                     label: const Text("Add Hit"),
                   )
