bcebfd89a5198eec9245fd750e1f75723dc81704
Juliette Cordor <professional@maybejules.com>
2024-03-24 23:12:33 +1100

    updates

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,18 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart'
     show FirebaseAuth, GoogleAuthProvider, UserCredential;
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart'
     show GoogleSignIn, GoogleSignInAccount, GoogleSignInAuthentication;
 
 Future<UserCredential> signInWithGoogle() async {
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  if (kIsWeb) {
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
 
-  final GoogleSignInAuthentication? googleAuth =
-      await googleUser?.authentication;
+    // googleProvider
+    //     .addScope('https://www.googleapis.com/auth/contacts.readonly');
+    // googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
 
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
+    return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+  } else {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  return FirebaseAuth.instance.signInWithCredential(credential);
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return FirebaseAuth.instance.signInWithCredential(credential);
+  }
 }
