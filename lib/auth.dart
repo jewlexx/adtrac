1e2cf036981fc072f51276202da366c06691fafa
Juliette Cordor <professional@maybejules.com>
2024-03-24 23:08:22 +1100

    move auth component into sign in page

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,32 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     show FirebaseAuth, GoogleAuthProvider, UserCredential;
 import 'package:google_sign_in/google_sign_in.dart'
     show GoogleSignIn, GoogleSignInAccount, GoogleSignInAuthentication;
 
 Future<UserCredential> signInWithGoogle() async {
-  // final googleUser = await GoogleSignIn(
-  //   signInOption: SignInOption.games,
-  // ).signIn();
-
-  // final googleAuth = await googleUser?.authentication;
-
-  // if (googleAuth != null) {
-  //   final credential = GoogleAuthProvider.credential(
-  //     accessToken: googleAuth.accessToken,
-  //     idToken: googleAuth.idToken,
-  //   );
-
-  //   return FirebaseAuth.instance.signInWithCredential(credential);
-  // } else {
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
   final GoogleSignInAuthentication? googleAuth =
       await googleUser?.authentication;
-
-  // if (googleAuth == null) {
-  //   throw Exception("Missing Auth");
-  // }
 
   final credential = GoogleAuthProvider.credential(
     accessToken: googleAuth?.accessToken,
@@ -34,81 +15,4 @@ Future<UserCredential> signInWithGoogle() async {
   );
 
   return FirebaseAuth.instance.signInWithCredential(credential);
-  // }
-}
-
-// Define a custom Form widget.
-class Auth extends StatefulWidget {
-  final Function(String? uid) setUid;
-
-  const Auth({super.key, required this.setUid});
-
-  @override
-  AuthState createState() {
-    return AuthState();
-  }
-}
-
-// Define a corresponding State class.
-// This class holds data related to the form.
-class AuthState extends State<Auth> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a `GlobalKey<FormState>`,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    final setUid = super.widget.setUid;
-
-    // Build a Form widget using the _formKey created above.
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        TextButton(
-          onPressed: () => signInWithGoogle().then((cred) {
-            setUid(cred.user?.uid);
-          }),
-          child: const Text("Sign In With Google"),
-        ),
-        Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
-            child: Column(
-              children: <Widget>[
-                // Add TextFormFields and ElevatedButton here.
-                TextFormField(
-                  autocorrect: false,
-                ),
-                TextFormField(
-                  autocorrect: false,
-                  obscureText: true,
-                  enableSuggestions: false,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Validate returns true if the form is valid, or false otherwise.
-                      if (_formKey.currentState!.validate()) {
-                        // If the form is valid, display a snackbar. In the real world,
-                        // you'd often call a server or save the information in a database.
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Signing In')),
-                        );
-                      }
-                    },
-                    child: const Text('Sign In'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  }
 }
--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -18,7 +18,70 @@ class _SignInPageState extends State<SignInPage> {
           setUid: (_) => Navigator.of(context).pushNamed("/"),
         ),
       ),
-      // bottomNavigationBar: const NavigationBottomBar(selectedPage: 0),
+    );
+  }
+}
+
+class Auth extends StatefulWidget {
+  final Function(String? uid) setUid;
+
+  const Auth({super.key, required this.setUid});
+
+  @override
+  AuthState createState() {
+    return AuthState();
+  }
+}
+
+class AuthState extends State<Auth> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final setUid = super.widget.setUid;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextButton(
+          onPressed: () => signInWithGoogle().then((cred) {
+            setUid(cred.user?.uid);
+          }),
+          child: const Text("Sign In With Google"),
+        ),
+        Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+            child: Column(
+              children: <Widget>[
+                // Add TextFormFields and ElevatedButton here.
+                TextFormField(
+                  autocorrect: false,
+                ),
+                TextFormField(
+                  autocorrect: false,
+                  obscureText: true,
+                  enableSuggestions: false,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Signing In')),
+                        );
+                      }
+                    },
+                    child: const Text('Sign In'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
